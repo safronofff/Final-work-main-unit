88ebb0ce465de93620caeba48055f341d60c03a9
safronofff <yuriy.safronoff@yandex.ru>
2023-11-24 15:08:08 +0300

    work_7

--- work/Program.cs
+++ work/Program.cs
@@ -15,3 +15,6 @@ Console.WriteLine("Hello, World!");
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
